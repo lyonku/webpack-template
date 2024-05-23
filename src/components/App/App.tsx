87176e676e12b26a1426ce808133591a3b603566
lyonku <z.pilipenko123@gmail.com>
2024-05-23 13:09:33 +0300

    add app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,13 +2,5 @@ import { useState } from "react";
 import "./App.css";
 
 export const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  return (
-    <div>
-      <div>Счетчик: {counter}</div>
-      <button onClick={() => setCounter((prev) => prev - 1)}>-</button>
-      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
-    </div>
-  );
+  return <div>GOGWARTS</div>;
 };
